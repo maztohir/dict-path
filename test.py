22cbe2f853c91d61e95e3e7d3d1c1ae8eea088c8
Muhamad Tohir <maztohir@gmail.com>
2023-09-04 23:13:55 +0700

    fix: support array on get and extract_dict

--- test.py
+++ test.py
@@ -13,6 +13,10 @@ def test_inject_value():
     inject_dict(test_dict, 'foo1/foo2/foo3', {'foo4':'bar'})
     assert extract_dict(test_dict, 'foo1/foo2/foo3/foo4') == 'bar'
 
+def test_extract_array():
+    test_dict = {'foo1':{'foo2':[{'foo3':'bar'},{'foo4':[[{'foo5': 'bar'}]]}]}}
+    assert extract_dict(test_dict, 'foo1/foo2/1/foo4/0/0/foo5') == 'bar'
+
 test_dict = {'foo1':{'foo2':{'foo3':{'foo4':'bar'}}}}
 data = DictPath(test_dict)
 
@@ -49,3 +53,13 @@ def test_equalness_with_normal_dict():
     dic_path = DictPath(normal_dict)
     assert normal_dict == dic_path
     assert dic_path.dict is normal_dict
+
+def test_extract_array_with_dict():
+    test_dict = {'foo1':{'foo2':[{'foo3':'bar'},{'foo4':[[{'foo5': 'bar'}]]}]}}
+    dic_path = DictPath(test_dict)
+    assert dic_path.get('foo1/foo2/1/foo4/0/0/foo5') == 'bar'
+
+def test_extract_array_with_dict():
+    test_dict = {'foo1':{'foo2':[{'foo3':'bar'},{'foo4':[[{'foo5': 'bar'}]]}]}}
+    dic_path = DictPath(test_dict)
+    assert dic_path.get('foo1/foo2/1/foo4/0/0/foo6/foo7') == None
