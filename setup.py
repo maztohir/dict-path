34347dc1d6ecd5848355ff4e8486c132847d63cb
Muhamad Tohir <maztohir@gmail.com>
2023-10-05 12:24:44 +0700

    feat: add gh workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dict-path",
-    version="1.0.1",
+    version="1.0.2",
     author="Muhamad Tohir",
     author_email="maztohir@gmail.com",
     description="Python library to work with complicated nested dict",
